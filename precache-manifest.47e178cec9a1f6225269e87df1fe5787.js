fa2685da28cdef61feb9a74b1355b2eb60837ad2
Jatin <mehtajatin47.jm@gmail.com>
2020-05-12 19:04:56 +0530

    Updates

---
+++ precache-manifest.47e178cec9a1f6225269e87df1fe5787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e925698a4cb585f42655f3f522599255",
+    "url": "/MonsterReactProject/index.html"
+  },
+  {
+    "revision": "032ec84af953719a9e8e",
+    "url": "/MonsterReactProject/static/css/main.6bf86fda.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/MonsterReactProject/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterReactProject/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032ec84af953719a9e8e",
+    "url": "/MonsterReactProject/static/js/main.e04ed8c5.chunk.js"
+  },
+  {
+    "revision": "72f8c5bb499949ba710e",
+    "url": "/MonsterReactProject/static/js/runtime-main.1df0f560.js"
+  }
+]);
